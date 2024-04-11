988d6e1e0c037c4ee61ad7e048bd243286191acd
Pisit Jaiton <pisit.j@arise.tech>
2024-04-11 14:05:32 +0700

    fix home

--- lib/feature/presentation/pages/home/home_page.dart
+++ lib/feature/presentation/pages/home/home_page.dart
@@ -2,8 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' as bloc;
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
@@ -21,15 +23,19 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
   final ScrollController _scrollController = ScrollController();
   double _opacity = 1;
   bool _appbar = false;
+  late TabController _tabController;
+  var chipList = ["Travel", "Beach", "Forest", "Mountain", "Thailand"];
+  var chipIndex = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
     _scrollController.addListener(() {
       print(_scrollController.offset);
       if (_scrollController.offset < 0) {
@@ -92,13 +98,12 @@ class _HomePageState extends State<HomePage> {
                       topRight: Radius.circular(32),
                     ),
                   ),
-                  child: Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 24, vertical: 3.h),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 3.h, left: 2.5.h,right: 2.5.h,bottom: 1.h),
+                        child: Text(
                           "Daily Journey",
                           style: TextStyle(
                             fontFamily: 'poppins',
@@ -106,9 +111,46 @@ class _HomePageState extends State<HomePage> {
                             fontWeight: FontWeight.w600,
                           ),
                         ),
-                        _buildBlocJourney()
-                      ],
-                    ),
+                      ),
+                      Container(
+                        height: 4.h,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 5,
+                            itemBuilder: (context,index){
+                          return GestureDetector(
+                            onTap: (){
+                               setState(() {
+                                 chipIndex = index;
+                               });
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 1.w, left: index == 0 ? 2.5.h : 0 ),
+                              child: Center(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: chipIndex == index ?Color(0xFF4D32F8) : Colors.white,
+                                    border: Border.all(color: chipIndex == index ?Color(0xFF4D32F8) : Color(0x03009C) .withOpacity(0.2)),
+                                    borderRadius: BorderRadius.circular(24)
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 1.w,),
+                                        Text(chipList[index],style: TextStyle(fontWeight: FontWeight.w600,color: chipIndex == index? Colors.white : Colors.black),),
+                                        SizedBox(width: 1.w,),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                      _buildBlocJourney()
+                    ],
                   ),
                 ),
               ),
@@ -128,19 +170,24 @@ class _HomePageState extends State<HomePage> {
             AnimatedSwitcher(
               duration: Duration(milliseconds: 300),
               child: _appbar
-                  ? Container(
-                      width: 100.w,
-                      child: Text(
-                        "Daily Journey",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.bold,
+                  ? GestureDetector(
+                onTap: (){
+                  _scrollController.animateTo(0, duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+                },
+                    child: Container(
+                        width: 100.w,
+                        child: Text(
+                          "Daily Journey",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
-                        textAlign: TextAlign.start,
                       ),
-                    )
+                  )
                   : Row(
                       children: [
                         Text(
@@ -173,9 +220,8 @@ class _HomePageState extends State<HomePage> {
                           child: GestureDetector(
                             onTap: () {
                               Get.to(
-                                transition: Transition.rightToLeft,
-                                  () => JourneyPlannerPage()
-                              );
+                                  transition: Transition.rightToLeft,
+                                  () => JourneyPlannerPage());
                             },
                             child: AnimatedContainer(
                               duration: Duration(milliseconds: 650),
@@ -185,17 +231,25 @@ class _HomePageState extends State<HomePage> {
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(16))),
                               child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 18),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 18),
                                 child: Row(
                                   children: [
                                     Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
                                       children: [
                                         Row(
                                           children: [
-                                            Image.asset(tripGenIcon ,scale: 3,),
-                                            SizedBox(width: 1.w,),
+                                            Image.asset(
+                                              tripGenIcon,
+                                              scale: 3,
+                                            ),
+                                            SizedBox(
+                                              width: 1.w,
+                                            ),
                                             Text(
                                               "Trip Generator",
                                               style: TextStyle(
@@ -206,11 +260,14 @@ class _HomePageState extends State<HomePage> {
                                             ),
                                           ],
                                         ),
-                                        SizedBox(height: 0.5.h,),
+                                        SizedBox(
+                                          height: 0.5.h,
+                                        ),
                                         Text(
                                           "Provide a youtube link \nthen get the trip powered by Gemini",
                                           style: TextStyle(
-                                              color: Colors.white.withOpacity(0.6),
+                                              color:
+                                                  Colors.white.withOpacity(0.6),
                                               fontFamily: 'inter',
                                               fontWeight: FontWeight.w400,
                                               fontSize: 13.sp),
@@ -218,8 +275,11 @@ class _HomePageState extends State<HomePage> {
                                       ],
                                     ),
                                     Spacer(),
-                                    Icon(EvaIcons.arrowIosForward,color: Colors.white,size: 24.sp,)
-
+                                    Icon(
+                                      EvaIcons.arrowIosForward,
+                                      color: Colors.white,
+                                      size: 24.sp,
+                                    )
                                   ],
                                 ),
                               ),
@@ -248,53 +308,101 @@ class _HomePageState extends State<HomePage> {
         );
       }
       if (state is HomeLoadedState) {
-        return GridView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 4.w,
-              crossAxisSpacing: 4.w,
-              childAspectRatio: 0.75),
-          itemCount: state.list!.items?.length,
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(12))),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(12),
-                        topLeft: Radius.circular(12)),
-                    child: CachedNetworkImage(
-                      imageUrl:
-                      state.list!.items![index].snippet!.thumbnails?.high?.url ?? "",
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Container(
-                        child: Text(
-                          state.list!.items![index].snippet!.title ?? "",
-                          style: TextStyle(
-                              fontFamily: 'inter',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 14.sp),
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
+        return Padding(
+          padding: EdgeInsets.only(left: 2.5.h,right: 2.5.h,bottom: 1.h),
+          child: MasonryGridView.builder(
+            padding: EdgeInsets.only(top: 2.h),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: state.list!.items?.length,
+              crossAxisSpacing: 2.w,
+              mainAxisSpacing: 1.h,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: Wrap(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(12),
+                            topLeft: Radius.circular(12)),
+                        child: CachedNetworkImage(
+                          imageUrl: state.list!.items![index].snippet!.thumbnails
+                                  ?.high?.url ??
+                              "",
+                          fit: BoxFit.fill,
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          },
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Container(
+                          child: Text(
+                            state.list!.items![index].snippet!.title ?? "",
+                            style: TextStyle(
+                                fontFamily: 'inter',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14.sp),
+                            maxLines: index % 3 == 0
+                                ? 3
+                                : index % 3 == 1
+                                    ? 2
+                                    : 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 1.h, right: 1.h),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Color(0xFFDBE4FD),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(24))),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 3.w, vertical: .5.h),
+                                child: Row(
+                                  children: [
+                                    Icon(EvaIcons.people,
+                                        size: 15.sp, color: Color(0xFF0034A0)),
+                                    SizedBox(
+                                      width: 1.w,
+                                    ),
+                                    ConstrainedBox(
+                                      constraints: BoxConstraints(
+                                        maxWidth: 25.w
+                                      ),
+                                      child: Text(
+                                        state.list!.items![index].snippet!
+                                            .channelTitle!,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Color(0xFF0034A0),
+                                            fontFamily: 'inter',
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 13.sp),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }),
         );
       }
 
