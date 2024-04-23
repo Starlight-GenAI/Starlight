bfab64652cb5c33f4a3dd9b40a36cc0ade754317
Pisit Jaiton <pisit.j@arise.tech>
2024-04-23 11:23:22 +0700

    Update constants.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,5 @@
 const String userProfileDefault = "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=";
-const String youtubeAPIKey = 'AIzaSyB6KBGJapD9CSF9a7QPwR9dqoTwOnsGGlI';
+const String youtubeAPIKey = '';
 const String youtubeAPIBaseURL = "https://youtube.googleapis.com/youtube/v3";
 const String uploadVideoBaseURL = "http://34.29.108.253:8080";
 const String starlightServiceBaseURL = "http://34.87.143.194:8080";
