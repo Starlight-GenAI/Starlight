d201e1d6e2220d155c38b2701b5416dd476bb220
Pisit Jaiton <pisit.j@arise.tech>
2024-05-03 01:09:00 +0700

    Update customize_preset_page.dart

--- lib/feature/presentation/pages/customize/customize_preset_page.dart
+++ lib/feature/presentation/pages/customize/customize_preset_page.dart
@@ -87,17 +87,18 @@ class _CustomizePresetPageState extends State<CustomizePresetPage> {
       },
       child: Scaffold(
         backgroundColor: Color(0xFFF9FBFE) ,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 1.h,vertical: 1.h),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 1.h,vertical: 1.h),
+                    child: SafeArea(
+                      bottom: false,
                       child: Row(
                         children: [
                           Container(
@@ -122,129 +123,128 @@ class _CustomizePresetPageState extends State<CustomizePresetPage> {
                       ),
                     ),
                   ),
+                ),
 
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 2.h,vertical: 2.h),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: 100.w,
-                          height: .8.h,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFEFF1F4),
-                            borderRadius: BorderRadius.circular(24)
-                          ),
-                        ),
-                        AnimatedContainer(
-                          width: 20.w * (_indexPage + 1),
-                          height: .8.h,
-                            duration: Duration(milliseconds: 400),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(24),
-                            color: Color(0xFF4D32F8)
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Column(
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 2.h,vertical: 2.h),
+                  child: Stack(
                     children: [
-                      Text('STEP ${(_indexPage+1)}/5',style: TextStyle(color: Color(0xFF4D32F8), fontFamily: 'inter', fontWeight: FontWeight.w700,letterSpacing: 1),),
-                      SizedBox(height: 1.h,),
-
-                      Text(pageHeader[_indexPage],style: TextStyle( fontFamily: 'poppins', fontWeight: FontWeight.w700, fontSize: 18.sp,),textAlign: TextAlign.center,),
-                      SizedBox(height: 2.h,),
-                      Text(pageTitle[_indexPage],style: TextStyle(color: Color(0xFF8C8C8C), fontFamily: 'inter', fontWeight: FontWeight.w500, fontSize: 15.sp,),textAlign: TextAlign.center,),
                       Container(
                         width: 100.w,
-                        height: 55.h,
-                        child: PageView(
-                          physics: const NeverScrollableScrollPhysics(),
-                          controller: pageController,
-                          children: [
-                            FirstPreset() , SecondPreset(), ThirdPreset(), PresetSelectCountry(), PresetSummaryResult()
-                          ],
-
+                        height: .8.h,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFEFF1F4),
+                          borderRadius: BorderRadius.circular(24)
                         ),
                       ),
-                      // Spacer(),
-                    ],
-
-                  ),
-
-
-                ],
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                left: 0,
-                child:
-              SafeArea(
-                top: false,
-                child: blocProvider.BlocProvider<JourneyPlannerBloc>(
-                  create: (context) => sl(),
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_indexPage < 4){
-                          setState(() {
-                            _indexPage++;
-                          });
-                          pageController.animateToPage(_indexPage,
-                              duration: Duration(milliseconds: 200),
-                              curve: Curves.linear);
-                        }else{
-                        blocProvider.BlocProvider.of<JourneyPlannerBloc>(context).add(UploadVideo(
-                            videoUrl: '',
-                            videoId: '',
-                            isUseSubtitle: false,
-                            userId: Get.find<NavigationController>().uid.value,
-                            promptPreset: PromptPresetRequestBody(
-                                day: Get.find<PresetController>().day.value,
-                                city: Get.find<PresetController>().location.value,
-                                journeyType: Get.find<PresetController>().comingWith.value,
-                                interestingActivity: Get.find<PresetController>().activities.value
-                            )
-                        ));
-                      }
-                      },
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 24),
-                      child: Container(
-                          width: 100.w,
-                          height: 15.w,
-                          decoration: const BoxDecoration(
-                              color: Color(0xFF4D32F8),
-                              borderRadius: BorderRadius.all(Radius.circular(100))
-                          ),
-                          child: Center(
-                              child: !isLoading ? Text(
-                                _indexPage==2? "Apply": "Next",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Poppins',
-                                    fontSize: 17.sp,
-                                    fontWeight: FontWeight.w800),
-                              ):
-                              LoadingAnimationWidget.discreteCircle(
-                              color: Colors.white,
-                              secondRingColor: Color(0xFF4D32F8),
-                              thirdRingColor:  Color(0xFF9AA6FF),
-                              size: 20.sp
-                          )
-
-                          )
+                      AnimatedContainer(
+                        width: 20.w * (_indexPage + 1),
+                        height: .8.h,
+                          duration: Duration(milliseconds: 400),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Color(0xFF4D32F8)
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+
+                Column(
+                  children: [
+                    Text('STEP ${(_indexPage+1)}/5',style: TextStyle(color: Color(0xFF4D32F8), fontFamily: 'inter', fontWeight: FontWeight.w700,letterSpacing: 1),),
+                    SizedBox(height: 1.h,),
+
+                    Text(pageHeader[_indexPage],style: TextStyle( fontFamily: 'poppins', fontWeight: FontWeight.w700, fontSize: 18.sp,),textAlign: TextAlign.center,),
+                    SizedBox(height: 2.h,),
+                    Text(pageTitle[_indexPage],style: TextStyle(color: Color(0xFF8C8C8C), fontFamily: 'inter', fontWeight: FontWeight.w500, fontSize: 15.sp,),textAlign: TextAlign.center,),
+                    Container(
+                      width: 100.w,
+                      height: 55.h,
+                      child: PageView(
+                        physics: const NeverScrollableScrollPhysics(),
+                        controller: pageController,
+                        children: [
+                          FirstPreset() , SecondPreset(), ThirdPreset(), PresetSelectCountry(), PresetSummaryResult()
+                        ],
+
+                      ),
+                    ),
+                    // Spacer(),
+                  ],
+
+                ),
+
+
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child:
+            SafeArea(
+              top: false,
+              child: blocProvider.BlocProvider<JourneyPlannerBloc>(
+                create: (context) => sl(),
+                child: GestureDetector(
+                  onTap: () {
+                    if (_indexPage < 4){
+                        setState(() {
+                          _indexPage++;
+                        });
+                        pageController.animateToPage(_indexPage,
+                            duration: Duration(milliseconds: 200),
+                            curve: Curves.linear);
+                      }else{
+                      blocProvider.BlocProvider.of<JourneyPlannerBloc>(context).add(UploadVideo(
+                          videoUrl: '',
+                          videoId: '',
+                          isUseSubtitle: false,
+                          userId: Get.find<NavigationController>().uid.value,
+                          promptPreset: PromptPresetRequestBody(
+                              day: Get.find<PresetController>().day.value,
+                              city: Get.find<PresetController>().location.value,
+                              journeyType: Get.find<PresetController>().comingWith.value,
+                              interestingActivity: Get.find<PresetController>().activities.value
+                          )
+                      ));
+                    }
+                    },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Container(
+                        width: 100.w,
+                        height: 15.w,
+                        decoration: const BoxDecoration(
+                            color: Color(0xFF4D32F8),
+                            borderRadius: BorderRadius.all(Radius.circular(100))
+                        ),
+                        child: Center(
+                            child: !isLoading ? Text(
+                              _indexPage==2? "Apply": "Next",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins',
+                                  fontSize: 17.sp,
+                                  fontWeight: FontWeight.w800),
+                            ):
+                            LoadingAnimationWidget.discreteCircle(
+                            color: Colors.white,
+                            secondRingColor: Color(0xFF4D32F8),
+                            thirdRingColor:  Color(0xFF9AA6FF),
+                            size: 20.sp
+                        )
+
+                        )
                     ),
                   ),
                 ),
               ),
-              )
-            ],
-          ),
-
+            ),
+            )
+          ],
         ),
       ),
     );
