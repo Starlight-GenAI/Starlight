1221600a81b3ec92f9148300cf8d797ad3c0a02d
Pisit Jaiton <pisit.j@arise.tech>
2024-05-03 07:48:41 +0700

    update constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,5 @@
-const String youtubeAPIKey = 'AIzaSyB6KBGJapD9CSF9a7QPwR9dqoTwOnsGGlI';
-const String placeAPIKey = 'AIzaSyBUWL3YL5gSpmRsS1Zjlp3ip9KA60Qd9I8';
+const String youtubeAPIKey = '';
+const String placeAPIKey = '';
 const String youtubeAPIBaseURL = "https://youtube.googleapis.com/youtube/v3";
 const String uploadVideoBaseURL = "http://34.29.108.253:8080";
 const String starlightServiceBaseURL = "http://34.87.143.194:8080";
